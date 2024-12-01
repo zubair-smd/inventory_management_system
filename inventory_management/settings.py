c09404d7faa598f94f3eb5419831f75aad147b36
zubair-smd <Zubaircdp23@gmail.com>
2024-12-02 00:00:51 +0530

    updated code

--- inventory_management/settings.py
+++ inventory_management/settings.py
@@ -55,7 +55,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.whiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'inventory_management.urls'
